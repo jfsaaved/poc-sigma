002f3eca8a6792176739015f3564176d95105f85
jfsaaved <julian.felipe.saavedra@gmail.com>
2020-01-31 15:03:06 -0500

    POM

--- src/main/java/com/sigma/pocsigma/repository/ImageRepository.java
+++ src/main/java/com/sigma/pocsigma/repository/ImageRepository.java
@@ -4,8 +4,6 @@ import com.sigma.pocsigma.model.Image;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface ImageRepository extends CrudRepository<Image, Integer> {
 
