2b52e5859467f367968f32e9f47f3746ed8d75f6
jfsaaved <julian.felipe.saavedra@gmail.com>
2020-01-31 10:35:16 -0500

    init

---
+++ src/main/java/com/sigma/pocsigma/PocSigmaApplication.java
@@ -0,0 +1,13 @@
+package com.sigma.pocsigma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocSigmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocSigmaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sigma/pocsigma/PocSigmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sigma.pocsigma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocSigmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
